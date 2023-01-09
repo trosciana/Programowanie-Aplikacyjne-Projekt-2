5b48267b1ef30d47b0cde2c0a263d61ecd573543
trosciana <103063033+trosciana@users.noreply.github.com>
2023-01-09 23:04:05 +0100

    Pierwszy commit
    Test bazy danych

---
+++ Projekt_2/Projekt_2/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Projekt_2
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string connectionString = "SERVER=localhost;DATABASE=data_test;UID=root;PASSWORD=Studentkazuzia1";
+
+            MySqlConnection connection = new MySqlConnection(connectionString);
+
+            MySqlCommand cmd = new MySqlCommand("SELECT * FROM data_data;", connection);
+            connection.Open();
+            DataTable dt = new DataTable();
+            dt.Load(cmd.ExecuteReader());
+            connection.Close();
+
+            dtGrid.DataContext = dt;   
+        }
+    }
+}
---
+++ Projekt_2/Projekt_2/dbo.Table.txt
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [a] DECIMAL NULL DEFAULT 1
+)
