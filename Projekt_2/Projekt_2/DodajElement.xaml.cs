1952ac9c50a75104ffbcd57228e9ff04d245da86
trosciana <103063033+trosciana@users.noreply.github.com>
2023-01-22 22:18:34 +0100

    final version

---
+++ Projekt_2/Projekt_2/DodajElement.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using System.Text.RegularExpressions;
+using System.Windows.Input;
+
+namespace Projekt_2
+{
+    /// <summary>
+    /// Logika interakcji dla klasy DodajElement.xaml
+    /// </summary>
+    public partial class DodajElement : Window
+    {
+        public DodajElement()
+        {
+            InitializeComponent();
+        }
+
+        public string danie
+        {
+            get { return DanieText.Text; }
+        }
+        public string kalorie
+        {
+            get { return KalorieText.Text; } //skopiowane, z okna jestem w stanie zwrócić informacje z jednego okna do drugiego - zwraca do linii 80 i 81
+        }
+
+        public void dodajDoBazy(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e) //skopiowane, tylko cyfry w polu kalorie
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+    }
+
+
+}
--- Projekt_2/Projekt_2/MainWindow.xaml.cs
+++ Projekt_2/Projekt_2/MainWindow.xaml.cs
@@ -1,20 +1,8 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Projekt_2
 {
@@ -24,7 +12,7 @@ namespace Projekt_2
     /// 
     public static class GLOBALS
     {
-        public static string connectionString { get; set; }
+        public static string connectionString { get; set; } 
         public static MySqlConnection connection { get; set; }
         public static string table { get; set; }
 
@@ -34,50 +22,71 @@ namespace Projekt_2
         public MainWindow()
         {
             InitializeComponent();
-            GLOBALS.table = "posilki";
-            GLOBALS.connectionString = "SERVER=localhost;DATABASE=data_test;UID=root;PASSWORD=123456789";
+            GLOBALS.table = "posilki"; //wynesione zeby nie musiec wszedzie kopiowac 
+            GLOBALS.connectionString = "SERVER=localhost;DATABASE=baza_danych_projekt;UID=root;PASSWORD=123456789"; //polaczenie z konkretna baza danych data_test
+            GLOBALS.connection = new MySqlConnection(GLOBALS.connectionString); //konektor do bazy
             dane.CanUserAddRows = false;  // to jest po to zeby zniknal pusty wiersz, defaultowo true
             OdswiezWidok();
         }
 
         private void OdswiezWidok()
         {
-            GLOBALS.connection = new MySqlConnection(GLOBALS.connectionString); //konektor do bazy
             MySqlCommand cmd = new MySqlCommand($"SELECT * FROM {GLOBALS.table}", GLOBALS.connection); //konkretne zapytanie sql które chce wykonać; odwołanie do bazy 
            
-            GLOBALS.connection.Open();
-            DataTable dt = new DataTable();
-            dt.Load(cmd.ExecuteReader());
+            GLOBALS.connection.Open(); 
+            DataTable dt = new DataTable(); //obsluga danych
+            dt.Load(cmd.ExecuteReader()); //wczytuje do data table to co zrobi command
+            dt.Columns.Add("SumaKalorii", typeof(Double));
+            int indeksWiersza = 0;
+            double sumaWszystko = 0;
+            foreach(DataRow row in dt.Rows)
+            {
+                double iloczynIloscKalorie = Convert.ToDouble(row[1]) * Convert.ToDouble(row[2]);
+                dt.Rows[indeksWiersza][3] = iloczynIloscKalorie;
+                sumaWszystko += iloczynIloscKalorie;
+                indeksWiersza++;
+            }
             GLOBALS.connection.Close();
-            dane.DataContext = dt;
+            dane.DataContext = dt; //wpisanie do komórki w okienku
+            SumaSuma.Text = sumaWszystko.ToString();
         }
 
         private void DodajIlosc(object sender, RoutedEventArgs e) //dodawanie ilosci w bazie
         {
-            if ((sender as Button).DataContext != null)
-            {
                 string nazwa = (sender as Button).DataContext.ToString();
                 MySqlCommand cmd = new MySqlCommand($"UPDATE {GLOBALS.table} SET ilosc = ilosc + 1 WHERE danie = '{nazwa}';", GLOBALS.connection);
                 GLOBALS.connection.Open();
                 cmd.ExecuteReader();
                 GLOBALS.connection.Close();
                 OdswiezWidok();
-            }
+          
         }
 
         private void OdejmijIlosc(object sender, RoutedEventArgs e) //odejmowanie ilosci w bazie
-        {
-            if ((sender as Button).DataContext != null)
             {
                 string nazwa = (sender as Button).DataContext.ToString();
-                MySqlCommand cmd = new MySqlCommand($"UPDATE {GLOBALS.table} SET ilosc = ilosc - 1 WHERE danie = '{nazwa}';", GLOBALS.connection);
+                MySqlCommand cmd = new MySqlCommand($"UPDATE {GLOBALS.table} SET ilosc = ilosc - 1 WHERE danie = '{nazwa}' AND ilosc > 0;", GLOBALS.connection);
+                GLOBALS.connection.Open();
+                cmd.ExecuteReader();
+                GLOBALS.connection.Close();
+                OdswiezWidok();
+        }
+
+        private void DodajElementDoBazy(object sender, RoutedEventArgs e)
+        {
+            DodajElement dodajElement = new DodajElement();
+            dodajElement.ShowDialog();
+            string danie = dodajElement.danie;
+            string kalorie = dodajElement.kalorie;
+
+            if (!String.IsNullOrEmpty(danie) & !String.IsNullOrEmpty(kalorie)) // jeśli danie i kalorie nie są puste
+            {
+                MySqlCommand cmd = new MySqlCommand($"INSERT INTO {GLOBALS.table} VALUE('{danie}',{kalorie},0);", GLOBALS.connection);
                 GLOBALS.connection.Open();
                 cmd.ExecuteReader();
                 GLOBALS.connection.Close();
                 OdswiezWidok();
             }
         }
-      
     }
-
 }
